2c36f6ee035fa79ced5fa7155059a87733837e10
jhaemin <io@jhaemin.com>
2020-10-01 23:39:33 +0900

    chore: use recoil state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import '@/styles/globals.scss'
 import { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <RecoilRoot>
       <Component {...pageProps} />
       <div id="actions-pop-up"></div>
-    </>
+    </RecoilRoot>
   )
 }
 
