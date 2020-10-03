70c6d57d3e980c69c788a9b8e127aba9e8f01adf
jhaemin <io@jhaemin.com>
2020-10-03 22:58:08 +0900

    feat: set popup when activated

--- src/components/WebsiteItem/index.tsx
+++ src/components/WebsiteItem/index.tsx
@@ -1,14 +1,41 @@
 import { WebsiteInfo } from '@/types'
 import classNames from 'classnames'
+import { useRef } from 'react'
+import { useSetRecoilState } from 'recoil'
+import { actionsPopUpStateAtom } from '../ActionsPopUp'
 import $ from './style.module.scss'
 
 export type WebsiteItemProps = WebsiteInfo
 
 const WebsiteItem: React.FC<WebsiteItemProps> = (props) => {
+  const actionBtnRef = useRef<HTMLDivElement>(null)
+
+  const setActionsPopUpState = useSetRecoilState(actionsPopUpStateAtom)
+
+  function onClickActionBtn(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    e.preventDefault()
+
+    const actionBtnRect = actionBtnRef.current?.getBoundingClientRect()
+
+    setActionsPopUpState((previous) => ({
+      ...previous,
+      isActive: true,
+      position: {
+        x: actionBtnRect?.left ?? 0,
+        y: actionBtnRect?.top ?? 0,
+      },
+      websiteInfo: {
+        name: props.name,
+        url: props.url,
+      },
+    }))
+  }
+
   return (
     <a
       className={$['link']}
-      href={props.link}
+      href={props.url}
+      title={props.name}
       target="_blank"
       rel="noopener noreferrer"
     >
@@ -17,7 +44,11 @@ const WebsiteItem: React.FC<WebsiteItemProps> = (props) => {
           <img className={$['icon']} src={props.imgSrc} alt={props.name} />
         </div>
         <h2 className={$['name']}>{props.name}</h2>
-        <div className={$['action-wrapper']}>
+        <div
+          ref={actionBtnRef}
+          className={$['action-wrapper']}
+          onClick={onClickActionBtn}
+        >
           <i className={classNames('f7-icons', $['action'])}>
             ellipsis_vertical
           </i>
