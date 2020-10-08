462762e83080f67901bab33b72a5d0ec5fd690c1
jhaemin <io@jhaemin.com>
2020-10-08 23:17:25 +0900

    fix: include global fonts stylesheets from _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
 import '@/styles/globals.scss'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <div className="background"></div>
-      <Component {...pageProps} />
-      <div id="actions-pop-up"></div>
-    </RecoilRoot>
+    <>
+      <Head>
+        <link rel="stylesheet" href="/fonts/framework7-icons.css" />
+        <link rel="stylesheet" href="/fonts/payw-pro.css" />
+      </Head>
+      <RecoilRoot>
+        <div className="background"></div>
+        <Component {...pageProps} />
+        <div id="actions-pop-up"></div>
+      </RecoilRoot>
+    </>
   )
 }
 
