9d8ea6ad8750796ed26af2463bc968c9d9c8ebfc
jhaemin <io@jhaemin.com>
2020-11-22 16:41:46 +0900

    feat: remove auth feature

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,9 @@
 import '@/styles/globals.scss'
-import { AuthData } from '@/types'
-import Cookies from 'cookies'
-import type { IncomingMessage, ServerResponse } from 'http'
-import App, { AppContext, AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 import Head from 'next/head'
-import { createContext } from 'react'
 import { RecoilRoot } from 'recoil'
 
-type WhereLandAppProps = AppProps & {
-  authData: AuthData | undefined
-}
-
-type AuthContextType = {
-  isSigned: boolean
-}
-
-const AuthContext = createContext<AuthContextType>({
-  isSigned: false,
-})
-
-function WhereLandApp({ Component, pageProps, authData }: WhereLandAppProps) {
+function WhereLandApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
@@ -48,82 +32,12 @@ function WhereLandApp({ Component, pageProps, authData }: WhereLandAppProps) {
         <meta name="theme-color" content="#ffffff" />
       </Head>
       <RecoilRoot>
-        {/* <AuthContext.Provider
-          value={{
-            isSigned: authData ? true : false,
-          }}
-        > */}
         <div className="background"></div>
         <Component {...pageProps} />
         <div id="actions-pop-up"></div>
-        {/* </AuthContext.Provider> */}
       </RecoilRoot>
     </>
   )
-}
-
-WhereLandApp.getInitialProps = async (appContext: AppContext) => {
-  const { ctx: context } = appContext
-
-  const cookies = new Cookies(
-    context.req as IncomingMessage,
-    context.res as ServerResponse
-  )
-
-  const accessToken = cookies.get('accessToken')
-
-  let authData: AuthData | undefined = undefined
-
-  if (accessToken) {
-    try {
-      // authData = verifyToken(accessToken)
-    } catch (err) {
-      if (err.name === 'TokenExpiredError') {
-        // Expired access token
-
-        console.log('Expired access token')
-        const refreshToken = cookies.get('refreshToken')
-
-        if (refreshToken) {
-          try {
-            // authData = verifyToken(refreshToken)
-
-            // const { data: user } = await Axios.get('/api/find-user', {
-            //   data: {
-            //     userID: authData.userID,
-            //   },
-            // })
-
-            // console.log(user)
-
-            // If the state of the refresh token has been changed,
-            // throw an error
-            // if (user?.refreshToken !== refreshToken) {
-            //   throw Error('Wrong refresh token')
-            // }
-
-            // const newAccessToken = signAccessToken(authData)
-            console.log('Signed new access token')
-
-            // Set the new access token to the cookie
-            // cookies.set('accessToken', newAccessToken, {
-            //   path: '/',
-            //   httpOnly: true,
-            // })
-          } catch (err) {
-            // Refresh token has been expired or malformed
-            console.log('Refresh token has expired or malformed')
-          }
-        }
-      } else {
-        // Malformed access token
-      }
-    }
-  }
-
-  const appProps = await App.getInitialProps(appContext)
-
-  return { ...appProps, authData }
 }
 
 export default WhereLandApp
