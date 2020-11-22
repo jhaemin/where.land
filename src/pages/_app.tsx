1d80f7ad7b2079436f92c7e8e6adeeb0676ab4d4
jhaemin <io@jhaemin.com>
2020-11-22 16:37:01 +0900

    feat: add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import '@/styles/globals.scss'
 import { AuthData } from '@/types'
-import Cookies from 'cookies'
-import { IncomingMessage, ServerResponse } from 'http'
-import App, { AppContext, AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { createContext } from 'react'
 import { RecoilRoot } from 'recoil'
@@ -25,6 +23,27 @@ function WhereLandApp({ Component, pageProps, authData }: WhereLandAppProps) {
       <Head>
         <link rel="stylesheet" href="/fonts/framework7-icons.css" />
         <link rel="stylesheet" href="/fonts/payw-pro.css" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <RecoilRoot>
         {/* <AuthContext.Provider
